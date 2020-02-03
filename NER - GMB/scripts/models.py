6b7131866fc97a45ac04deac704e6583f041a0c8
Ruslan_Golubev <ruslan_golubev@epam.com>
2020-02-03 18:56:46 +0300

    Update models.py

--- NER - GMB/scripts/models.py
+++ NER - GMB/scripts/models.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from tqdm import tqdm_notebook
 from hmmlearn import hmm
+import json
+import pickle
 
 
 class CrfFeatures:
@@ -132,6 +134,104 @@ class HMM(hmm.MultinomialHMM):
         prob = full_tags_list / sum(full_tags_list)
 
         return prob
+
+
+class SpacyFit:
+    # Convert .tsv file to dataturks json format.
+
+    def __init__(self):
+        pass
+
+    def tsv_to_json(self, input_path, output_path, unknown_label):
+
+        f = open(input_path, 'r', encoding='utf-8')  # input file
+        fp = open(output_path, 'w')  # output file
+        data_dict = {}
+        annotations = []
+        label_dict = {}
+        s = ''
+        start = 0
+        for line in f:
+            if line[0:len(line) - 1] != '.\tO':
+                word, entity = line.split('\t')
+                s += word + " "
+                entity = entity[:len(entity) - 1]
+                if entity != unknown_label:
+                    if len(entity) != 1:
+                        d = dict()
+                        d['text'] = word
+                        d['start'] = start
+                        d['end'] = start + len(word) - 1
+                        try:
+                            label_dict[entity].append(d)
+                        except:
+                            label_dict[entity] = []
+                            label_dict[entity].append(d)
+                start += len(word) + 1
+            else:
+                data_dict['content'] = s
+                s = ''
+                label_list = []
+                for ents in list(label_dict.keys()):
+                    for i in range(len(label_dict[ents])):
+                        if label_dict[ents][i]['text'] != '':
+                            l = [ents, label_dict[ents][i]]
+                            for j in range(i + 1, len(label_dict[ents])):
+                                if label_dict[ents][i]['text'] == label_dict[ents][j]['text']:
+                                    di = dict()
+                                    di['start'] = label_dict[ents][j]['start']
+                                    di['end'] = label_dict[ents][j]['end']
+                                    di['text'] = label_dict[ents][i]['text']
+                                    l.append(di)
+                                    label_dict[ents][j]['text'] = ''
+                            label_list.append(l)
+
+                for entities in label_list:
+                    label = dict()
+                    label['label'] = [entities[0]]
+                    label['points'] = entities[1:]
+                    annotations.append(label)
+                data_dict['annotation'] = annotations
+                annotations = []
+                json.dump(data_dict, fp)
+                fp.write('\n')
+                data_dict = {}
+                start = 0
+                label_dict = {}
+
+    def spacy_corpus(self, input_file=None, output_file=None):
+
+        training_data = []
+        with open(input_file, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            data = json.loads(line)
+            text = data['content']
+            entities = []
+            for annotation in data['annotation']:
+                point = annotation['points'][0]
+                labels = annotation['label']
+                if not isinstance(labels, list):
+                    labels = [labels]
+
+                for label in labels:
+                    entities.append((point['start'], point['end'] + 1, label))
+
+            training_data.append((text, {"entities": entities}))
+
+        with open(output_file, 'wb') as fp:
+            pickle.dump(training_data, fp)
+
+        return training_data
+
+    def run(self, data):
+
+        data.to_csv('ner_corpus.tsv', sep='\t', index=False, header=False)
+        self.tsv_to_json("ner_corpus.tsv", 'ner_corpus.json', 'abc')
+        spacy_input = self.spacy_corpus(input_file='ner_corpus.json', output_file='ner_corpus')
+        spacy_input = [sent for sent in spacy_input if sent[0] != '']
+        return spacy_input
 
 
 
